addda49748f15f36dedcba213db55561d83969eb
Michael Primavera <pastasauce59@gmail.com>
2021-05-19 14:21:00 -0400

    work in progress

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
 class Owner
-  # code goes here
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def species
+    "human"
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+
 end
