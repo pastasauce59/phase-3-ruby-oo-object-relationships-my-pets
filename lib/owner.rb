b521a3d3d9602975d8da4d38dd8004959e87b127
Isaac <izaroot327@gmail.com>
2021-05-19 14:46:55 -0400

    Completed Assignment

--- lib/owner.rb
+++ lib/owner.rb
@@ -15,6 +15,53 @@ class Owner
     "I am a #{species}."
   end
 
+  def cats
+    Cat.all.select do |cat_instance|
+      cat_instance.owner == self
+      end
+  end
+
+  def dogs
+    Dog.all.select do |dog_instance|
+      dog_instance.owner == self
+      end
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each do |dog|
+      dog.mood = "happy"
+      end
+  end
+
+  def feed_cats
+    self.cats.each do |cat|
+      cat.mood = "happy"
+      end
+  end
+
+  def sell_pets
+    self.cats.each do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+    self.dogs.each do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
   def self.all
     @@all 
   end
